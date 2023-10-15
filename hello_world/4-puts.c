7665d3fcaf52cd224857c130bb42c10724dabba2
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-14 21:46:50 -0400

    task 4 documentation

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,3 +5,10 @@ int main(void)
  puts("\"Programming is like building a multilingual puzzle");
  return 0;
 }
+
+/*
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
