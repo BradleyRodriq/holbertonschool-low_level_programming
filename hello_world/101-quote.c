ec2707d7541b95c36f86a40fa94a9dceba95c858
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-15 23:50:42 -0400

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <uninstd.h>
+#include <unistd.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -9,8 +9,8 @@
 int main(void)
 {
 	write(2,
-	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	 59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
