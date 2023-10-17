700739f150ee855ac823959f68ace0887744b68e
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-17 13:29:24 -0400

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: always 0
+ */
+
+int main (void)
+{
+	int  n;
+	n = 0;
+	
+	while (n < 10)
+	{	
+	printf("%d", n);
+	n++;
+	}
+	printf("\n");
+
+return (0);
+}
