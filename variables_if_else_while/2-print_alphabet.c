a0dec30ee72316cd7af09c1fb5c76774f9a6f4c9
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-17 11:01:39 -0400

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z') {
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');  
+	return 0;
+}
