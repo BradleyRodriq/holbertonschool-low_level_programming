01da99833050b8b1ab1a6218be3e3f6a3d4dd417
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-20 21:45:07 -0400

    task 11 betty

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: variable to start loop
+ * Return: prints all numbers up to 98
+ */
 void print_to_98(int n)
-
 {
 	if (n >= 98)
 	{
