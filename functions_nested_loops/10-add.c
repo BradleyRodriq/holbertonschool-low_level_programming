91f531bb1e10c168a099ba594673b1ae6d5f405c
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-20 21:01:27 -0400

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds up two integers
+ * @num1: The first integer added
+ * @num2: The second integer added
+ *
+ * Return: the result of the additions
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
