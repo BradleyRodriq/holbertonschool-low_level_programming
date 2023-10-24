76263d9407099f197657d5f2e4e1c1a005efc79e
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-24 09:20:37 -0400

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{ 
+		_putchar(s[i]);
+	}
+		i++;
+	i--;
+	while (i>= 0)
+	{
+		_putchar (s[i]);
+		i--;
+	}
+	_putchar ('\n');
+}
