ec919af34357a5a5e900cce3f1a26cd4999a5df5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-25 13:44:56 -0400

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,28 +4,28 @@
  * _strncat - merges two strings together
  * @dest: the base merge string
  * @src: the string to be merged into dest
- * n: limit of characters
+ * @n: limit of characters
  * Return: merged string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-		int i;
-		int j;
+	int i;
+	int j;
 
-		i = 0;
-		j = 0;
+	i = 0;
+	j = 0;
 
-        while (dest[i])
-        {
-                i++;
-        }
-        while (src[j] && j < n)
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
-        dest [i] = '\0';
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j] && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
-        return (dest);
+	return (dest);
 }
