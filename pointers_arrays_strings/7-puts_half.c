7ff27daebd81627d0086f58f220ed8791413c0f7
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-24 21:53:30 -0400

    task 7 maybe

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,6 +2,7 @@
 /**
  * puts_half - prints half of a string
  * @str: the string to be halved
+ * @l: length of string
  */
 
 void puts_half(char *str)
@@ -13,15 +14,13 @@ void puts_half(char *str)
 	{
 		l++;
 	}
+	{
 	if (l % 2 == 0)
-	{
 		l = l / 2;
-	}
 	else if (l % 2 != 0)
-	{
 		l = (l - 1) / 2;
-	}
 	while (str[l])
+	}
 	{
 		_putchar(str[l]);
 		l++;
