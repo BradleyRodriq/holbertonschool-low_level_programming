4b5772223d7a4813bc4dd6606588779f03bc88be
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-25 11:19:18 -0400

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _strcat - merges two strings together
+ * @dest: the base merge string
+ * @src: the string to be merged into dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *full_dest = dest;
