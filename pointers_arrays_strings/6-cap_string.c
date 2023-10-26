ce51cb532bdf84fd4289aeaaa7b1cbd5d51a5216
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-26 16:49:17 -0400

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * string_toupper - Changes all lowercase letters
- *                  of a string to uppercase.
- * @str: The string to be changed.
+ * cap_string - capitalizes all words of a string
+ * @str: The string to be capped
  *
- * Return: A pointer to the changed string.
+ * Return: changed string
  */
 char *cap_string(char *str)
 {
@@ -16,24 +15,21 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 
-		if (str[i] - 1 == ' ' ||
-				str[i] - 1 == '\t' ||
-				str[i] - 1 == '\n' ||
-				str[i] - 1 == ',' ||
-				str[i] - 1 == ';' ||
-				str[i] - 1 == '.' ||
-				str[i] - 1 == '!' ||
-				str[i] - 1 == '?' ||
-				str[i] - 1 == '"' ||
-				str[i] - 1 == '(' ||
-				str[i] - 1 == ')' ||
-				str[i] - 1 == '{' ||
-				str[i] - 1 == '}' ||
-				str[i] - 1 == '[' ||
-				str[i] - 1 == ']' ||
-				str[i] - 1 == ' ' ||
-				i == 0)
-			str[i] -= 32;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
+				str[i] -= 32;
 		i++;
 	}
 	return (str);
