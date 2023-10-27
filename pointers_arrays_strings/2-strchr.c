4e2f939e558b1bba53b82d0a194207b71107f545
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-27 14:27:58 -0400

    task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- *
+ * _strchr - locates a character in a string
+ * @s: the source string
+ * @c: the character to be located within s string
+ * Return: pointer of c onwards
  */
 char *_strchr(char *s, char c)
-
 {
 	while (*s)
 	{
