fc2d3df33784956d0079fbf244417614230b8b00
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-10-29 09:59:46 -0400

    task 2 fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,18 @@
  */
 char *_strchr(char *s, char c)
 {
+	if (*s == '\0')
+		return (s);
 	while (*s)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
+
 		s++;
 	}
+	if (c == '\0')
+		return (s);
 	return ('\0');
 }
