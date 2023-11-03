80ab87219bb6b3b19539edef64bf4d04ce330d54
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-02 22:21:56 -0400

    square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * sqrt2 - helper function to find square root
+ * @a: same value as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1, or -1 on error
+ */
+int _sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (_sqrt(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returs narutal square root of n
+ * @n: number to be squared
+ * Return: On success 1, or -1 on error
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
