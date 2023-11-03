9bd1f1a3dad87e22ee3917d815ba07baf40d0e2f
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-03 13:32:02 -0400

    multiply

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int produce;
+	int a;
+	int b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	produce = a * b;
+	{
+		printf("%d\n", produce);
+	}
+	return (0);
+}
