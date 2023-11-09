191ada3e639024b64b6a8617a575b7e98bd53056
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-09 11:33:11 -0400

    task 2 fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,14 +12,6 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	int i, concat_i = 0, l = 0;
 
-	if (s1 == NULL)
-	{
-	s1 = " ";
-	}
-	if (s2 == NULL)
-	{
-	s2 = " ";
-	}
 	for (i = 0; s1[i]; i++)
 	{
 		l++;
