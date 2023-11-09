06d7609018e002d32c8d64e33dffca521156f613
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-09 15:56:31 -0400

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		l++;
 	}
-	concat = malloc(sizeof(char) * l);
+	concat = malloc(sizeof(char) * l + 1);
 	if (concat == NULL)
 	{
 		return (NULL);
