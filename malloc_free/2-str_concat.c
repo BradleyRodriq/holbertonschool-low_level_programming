b17e9ac6261efeafdf2da884b5ad769b4e3c0d0b
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-09 11:22:45 -0400

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	int i, concat_i = 0, l = 0;
+
+	if (s1 == NULL)
+	{
+	s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+	s2 = " ";
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		l++;
+	}
+	for (i = 0; s2 [i]; i++)
+	{
+		l++;
+	}
+	concat_str = malloc(sizeof(char) * l);
+	if (concat_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		concat_str[concat_i++] = s1[i];
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		concat_str[concat_i++] = s2[i];
+	}
+	return (concat_str);
+}
