58665ffe2f428cf7feeb96cd96826f72a7bd663c
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-14 15:29:42 -0400

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * new_dog - create new dog
+ * @name - new dog name
+ * @age - new dog age
+ * @owner - new dog owner
+ *
+ * Return: new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
