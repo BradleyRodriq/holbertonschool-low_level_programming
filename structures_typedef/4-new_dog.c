16f1808571bf50bf7f4e0212b80b7cebf95be1de
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-14 15:44:15 -0400

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 
 /**
+ * _strncpy - a function ...
+ * @dest: the chaine
+ * @src: the chaine
+ * @n: the number
+ *
+ * Return: 1 or 0
+ */
+
+char	*_strncpy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/**
  * new_dog - create new dog
  * @name - new dog name
  * @age - new dog age
