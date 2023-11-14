f9f66f7effef72cff118d0283f57b80df1bca476
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-14 15:33:43 -0400

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,33 +11,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggo;
+	dog_t *ndog;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-	doggo = malloc(sizeof(dog_t));
-	if (doggo == NULL)
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
 		return (NULL);
 
-	doggo->name = malloc(sizeof(char*) * *name);
-	if (doggo->name == NULL)
+	ndog->name = malloc(sizeof(char*) * *name);
+	if (ndog->name == NULL)
 	{
-		free(doggo);
+		free(ndog);
 		return (NULL);
 	}
 
-	doggo->owner = malloc(sizeof(char*) * *owner);
-	if (doggo->owner == NULL)
+	ndog->owner = malloc(sizeof(char*) * *owner);
+	if (ndog->owner == NULL)
 	{
-		free(doggo->name);
-		free(doggo);
+		free(ndog->name);
+		free(ndog);
 		return (NULL);
 	}
 
-	doggo->name = name;
-	doggo->age = age;
-	doggo->owner = owner;
+	ndog->name = name;
+	ndog->age = age;
+	ndog->owner = owner;
 
-	return (doggo);
+	return (ndog);
 }
