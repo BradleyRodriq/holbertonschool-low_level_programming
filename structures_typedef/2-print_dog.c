900ddb5b30d0d4afe48f89282706bf93e16d8d97
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-14 14:18:51 -0400

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,20 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_dog - prints dog values
+ * @d: dog to be valued
+ */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
+	{
 		printf("Name: (Nil)\n");
+	}
 	else
+	{
 		printf("Name: %s\n", d->name);
+	}
 	if (d->age < 0)
+	{
 		printf("Age: (Nil)\n");
+	}
 	else
+	{
 		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
+	{
 		printf("Owner: (Nil)\n");
+	}
 	else
+	{
 		printf("Owner: %s\n", d->owner);
+	}
 }
