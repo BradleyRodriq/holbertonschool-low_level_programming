3083a3d97a815e202e3848f82040ecab6ad505b9
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-15 23:10:58 -0400

    task 1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNC_H
 #define FUNC_H
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
