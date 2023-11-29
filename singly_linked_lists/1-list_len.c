3b4da9ef732f4fa77a37cdb13d725baf897e5904
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-29 00:45:02 -0400

    element count

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * list_len - lists number of elements
+ * @h: input to be checked
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	size_t element_count = 0;
