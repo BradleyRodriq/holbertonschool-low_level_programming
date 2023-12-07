1188e5ea4c3ebac3042fcf6e85e19dbc35bf0f03
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-12-07 11:21:40 -0400

    task 0 readfile

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,17 +6,19 @@
  */
 void print_square(int size)
 {
-	int x, y;
+	int x;
+	int y;
 
 	if (size <= 0)
 		_putchar('\n');
-	for (x = 0; x < size; x++)
 	{
-		for (y = 0; y < size; y++)
+		for (x = 0; x < size; x++)
 		{
-			_putchar('#');
-		}
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
 		_putchar('\n');
+		}
 	}
-
 }
