61bee0bb061e781c75097a358053028edfee5dc2
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-12-10 14:59:02 -0400

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	}
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(from) == -1 || close(to) == -1)
