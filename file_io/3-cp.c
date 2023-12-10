6ee477b7e2f330de0713ab7de0308c53bf807a05
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-12-10 14:28:46 -0400

    added malloc fixed value

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,32 +6,37 @@ int main(int argc, char *argv[])
 {
 	int from, to, r, w;
 	char *buffer;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = malloc(sizeof(1024));
+
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (from == -1 || r == -1)
+	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	w = write(to, buffer, r);
-	if (to == -1 || w == -1)
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_WRONLY | O_APPEND);
+	
+	while ((r = read(from, buffer, 1024)) > 0)
+	{
+		w = write(to, buffer, r);
+		if (w == -1)
+		{
+			dprintf("Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 
-	free(buffer);
-	while (r > 0);
 	close(from);
 	close(to);
+
 	return (0);
 }
